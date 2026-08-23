a7b5c7f13d169c16cfaab364a0c17338d0700b5b
Adabala Satya Sai <saiadabala53@gmail.com>
2026-08-23 12:06:46 +0530

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("", include("core.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.BASE_DIR / "static")
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
